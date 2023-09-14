763682a69ad8fb4bcdae6c3a032f07f51ea7c29f
ikeicg <ikechukwuemeka60@gmail.com>
2023-09-14 17:13:06 +0100

    made additions

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+express = require("express");
+app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send("We are Back!")
+})
+
+app.listen(port, () => {
+    console.log(`Server is Running!!`);
+})
+
+
 
